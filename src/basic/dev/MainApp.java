a53d4f54aef439877a3d64973e2e1f6b10bb64b3
trandacga <gatran1402@gmail.com>
2021-04-22 15:29:37 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// tạo thông tin student
+		String name;
+		int id;
+		String cmnd;
+		String nameclass;
+		String nameschool;
+		float dtb;
+		// tạo thông tin xe car
+		String namecar;
+		String nameManufactuner;
+		float price;
+		String color;
+		// tạo thông tin Worker
+		String nameWorker;
+		String nameaddress;
+		String phone;
+		float coefficient;
+		
+
+	}
+
+}
